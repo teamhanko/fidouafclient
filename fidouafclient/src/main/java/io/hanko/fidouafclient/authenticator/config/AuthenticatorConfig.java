0dc82673b528b9507be8a02c04f6015343ba7dc3
Frederic Jahn <fja@cap3.de>
2019-07-19 11:27:05 +0200

    change aaid

--- fidouafclient/src/main/java/io/hanko/fidouafclient/authenticator/config/AuthenticatorConfig.java
+++ fidouafclient/src/main/java/io/hanko/fidouafclient/authenticator/config/AuthenticatorConfig.java
@@ -10,7 +10,7 @@ public class AuthenticatorConfig {
 
     public static Authenticator authenticator_fingerprint = new Authenticator(
             "Hanko Fido UAF Authenticator (Fingerprint)",
-            "C3C3#0001",
+            "A4A4#0001",
             "UAF Fingerprint Client/Authenticator from Hanko",
             supportedVersions,
             "UAFV1TLV",
@@ -30,7 +30,7 @@ public class AuthenticatorConfig {
 
     public static Authenticator authenticator_lockscreen = new Authenticator(
             "Hanko FIDO UAF Authenticator (Lockscreen)",
-            "C3C3#0003",
+            "A4A4#0003",
             "UAF Lockscreen Client/Authenticator from Hanko",
             supportedVersions,
             "UAFV1TLV",
